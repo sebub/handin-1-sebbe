ae81a76b91c80b5d61c9b3f90864503fc0044d49
sebub <105357150+sebub@users.noreply.github.com>
2022-09-09 23:38:15 +0200

    Update handin1.c

--- src/handin1.c
+++ src/handin1.c
@@ -1,5 +1,27 @@
-
+#include <stdio.h>
 int main(void)
 {
+    int seconds = 0, minutes = 0, hours = 0, days = 0, weeks = 0;
+    printf("Hello there please enter a number of seconds: ");
+    scanf("%d", &seconds);
+    printf("You have entered %d seconds\n", seconds);
+
+    while(seconds>=604800){
+        seconds -= 604800;
+        weeks += 1;
+    }
+    while(seconds>=86400){
+        seconds -= 86400;
+        days += 1;
+    }
+    while(seconds>=3600){
+        seconds -= 3600;
+        hours += 1;
+    }
+    while(seconds>=60){
+        seconds -= 60;
+        minutes += 1;
+    }
+    printf("weeks: %d, days: %d, hours: %d, minutes: %d, seconds: %d",weeks,days,hours,minutes,seconds);
 
 }
