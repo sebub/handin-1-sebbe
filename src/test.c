7ec45a17272837561e131c548b66656fda5252b1
sebub <105357150+sebub@users.noreply.github.com>
2022-09-11 00:00:59 +0200

    Delete test.c

--- src/test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(void)
-{
-    int seconds = 0, minutes = 0, hours = 0, days = 0, weeks = 0;
-    printf("Please enter a number of seconds:");
-    scanf("%d",&seconds);
-    printf("%d",seconds);
-}
-
